123687ba564af126c9b7316c0f3b9f72e0bf0afe
Andrew McRae <amcrae@google.com>
2024-06-11 09:52:32 +1000

    fetcher: Add Ready()

--- fetcher.go
+++ fetcher.go
@@ -3,6 +3,7 @@ package html
 import (
 	"errors"
 	"sync"
+	"sync/atomic"
 
 	"syscall/js"
 )
@@ -17,6 +18,7 @@ type Fetcher struct {
 	errF js.Func
 	respF js.Func
 	readyF js.Func
+	ready atomic.Bool
 }
 
 // NewFetcher returns a new instance of a fetcher.
@@ -27,7 +29,7 @@ func NewFetcher(w *Window, url string) * Fetcher{
 	f.wg.Add(1)
 	f.respF = js.FuncOf(f.response)
 	f.errF = js.FuncOf(f.reject)
-	f.readyF = js.FuncOf(f.ready)
+	f.readyF = js.FuncOf(f.dataReady)
 	fPromise.Call("then", f.respF, f.errF)
 	return f
 }
@@ -42,9 +44,15 @@ func (f *Fetcher) Get() ([]byte, error) {
 	return f.data, f.err
 }
 
+// Ready returns true if Get will succeed without block
+func (f *Fetcher) Ready() bool {
+	return f.ready.Load()
+}
+
 func (f *Fetcher) reject(this js.Value, args []js.Value) any {
 	f.err = errors.New("Rejected")
 	f.wg.Done()
+	f.ready.Store(true)
 	return nil
 }
 
@@ -59,11 +67,13 @@ func (f *Fetcher) response(this js.Value, args []js.Value) any {
 	// The file could not be accessed.
 	f.err = errors.New(js.ValueOf(args[0].Get("status")).String())
 	f.wg.Done()
+	f.ready.Store(true)
 	return nil
 }
 
-func (f *Fetcher) ready(this js.Value, args []js.Value) any {
+func (f *Fetcher) dataReady(this js.Value, args []js.Value) any {
 	f.data = []byte(js.ValueOf(args[0]).String())
 	f.wg.Done()
+	f.ready.Store(true)
 	return nil
 }

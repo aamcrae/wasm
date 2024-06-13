1bacdac6d9902e599c6d3ef6391ae378191f5e69
Andrew McRae <amcrae@google.com>
2024-06-13 17:44:11 +1000

    Pu tags in alphabetical order

--- tags.go
+++ tags.go
@@ -1,6 +1,106 @@
 // DO NOT EDIT - generated file
 package wasm
 
+func (h *HTML) Br(elems ...any) *frag {
+	return emptyTag("br", elems)
+}
+
+func (h *HTML) Hr(elems ...any) *frag {
+	return emptyTag("hr", elems)
+}
+
+func (h *HTML) Img(elems ...any) *frag {
+	return emptyTag("img", elems)
+}
+
+func (h *HTML) Link(elems ...any) *frag {
+	return emptyTag("link", elems)
+}
+
+func (h *HTML) Alt(elems ...any) attr {
+	return attribute("alt", elems)
+}
+
+func (h *HTML) Border(elems ...any) attr {
+	return attribute("border", elems)
+}
+
+func (h *HTML) Class(elems ...any) attr {
+	return attribute("class", elems)
+}
+
+func (h *HTML) For(elems ...any) attr {
+	return attribute("for", elems)
+}
+
+func (h *HTML) Href(elems ...any) attr {
+	return attribute("href", elems)
+}
+
+func (h *HTML) Id(elems ...any) attr {
+	return attribute("id", elems)
+}
+
+func (h *HTML) Name(elems ...any) attr {
+	return attribute("name", elems)
+}
+
+func (h *HTML) Onclick(elems ...any) attr {
+	return attribute("onclick", elems)
+}
+
+func (h *HTML) Onsubmit(elems ...any) attr {
+	return attribute("onsubmit", elems)
+}
+
+func (h *HTML) Rel(elems ...any) attr {
+	return attribute("rel", elems)
+}
+
+func (h *HTML) Size(elems ...any) attr {
+	return attribute("size", elems)
+}
+
+func (h *HTML) Src(elems ...any) attr {
+	return attribute("src", elems)
+}
+
+func (h *HTML) Style(elems ...any) attr {
+	return attribute("style", elems)
+}
+
+func (h *HTML) Summary(elems ...any) attr {
+	return attribute("summary", elems)
+}
+
+func (h *HTML) Title(elems ...any) attr {
+	return attribute("title", elems)
+}
+
+func (h *HTML) Type(elems ...any) attr {
+	return attribute("type", elems)
+}
+
+func (h *HTML) Value(elems ...any) attr {
+	return attribute("value", elems)
+}
+
+func (h *HTML) Download(elems ...any) attr {
+	return attrNoArg("download", elems)
+}
+
+func (h *HTML) A(elems ...any) *frag {
+	return tag("a", elems)
+}
+
+func (h *HTML) Div(elems ...any) *frag {
+	return tag("div", elems)
+}
+
+func (h *HTML) Form(elems ...any) *frag {
+	return tag("form", elems)
+}
+
 func (h *HTML) H1(elems ...any) *frag {
 	return tag("h1", elems)
 }
@@ -25,40 +125,28 @@ func (h *HTML) H6(elems ...any) *frag {
 	return tag("h6", elems)
 }
 
-func (h *HTML) Div(elems ...any) *frag {
-	return tag("div", elems)
-}
-
-func (h *HTML) A(elems ...any) *frag {
-	return tag("a", elems)
-}
-
-func (h *HTML) Span(elems ...any) *frag {
-	return tag("span", elems)
-}
-
-func (h *HTML) Ol(elems ...any) *frag {
-	return tag("ol", elems)
-}
-
-func (h *HTML) Form(elems ...any) *frag {
-	return tag("form", elems)
+func (h *HTML) Input(elems ...any) *frag {
+	return tag("input", elems)
 }
 
 func (h *HTML) Label(elems ...any) *frag {
 	return tag("label", elems)
 }
 
-func (h *HTML) Input(elems ...any) *frag {
-	return tag("input", elems)
-}
-
-func (h *HTML) Ul(elems ...any) *frag {
-	return tag("ul", elems)
-}
-
 func (h *HTML) Li(elems ...any) *frag {
 	return tag("li", elems)
+}
+
+func (h *HTML) Ol(elems ...any) *frag {
+	return tag("ol", elems)
+}
+
+func (h *HTML) P(elems ...any) *frag {
+	return tag("p", elems)
+}
+
+func (h *HTML) Span(elems ...any) *frag {
+	return tag("span", elems)
 }
 
 func (h *HTML) Table(elems ...any) *frag {
@@ -69,103 +157,15 @@ func (h *HTML) Tbody(elems ...any) *frag {
 	return tag("tbody", elems)
 }
 
-func (h *HTML) Tr(elems ...any) *frag {
-	return tag("tr", elems)
-}
-
 func (h *HTML) Td(elems ...any) *frag {
 	return tag("td", elems)
 }
 
-func (h *HTML) P(elems ...any) *frag {
-	return tag("p", elems)
+func (h *HTML) Tr(elems ...any) *frag {
+	return tag("tr", elems)
 }
 
-func (h *HTML) Br(elems ...any) *frag {
-	return emptyTag("br", elems)
-}
-
-func (h *HTML) Hr(elems ...any) *frag {
-	return emptyTag("hr", elems)
-}
-
-func (h *HTML) Link(elems ...any) *frag {
-	return emptyTag("link", elems)
-}
-
-func (h *HTML) Img(elems ...any) *frag {
-	return emptyTag("img", elems)
-}
-
-func (h *HTML) Alt(elems ...any) attr {
-	return attribute("alt", elems)
-}
-
-func (h *HTML) Title(elems ...any) attr {
-	return attribute("title", elems)
-}
-
-func (h *HTML) Src(elems ...any) attr {
-	return attribute("src", elems)
-}
-
-func (h *HTML) For(elems ...any) attr {
-	return attribute("for", elems)
-}
-
-func (h *HTML) Name(elems ...any) attr {
-	return attribute("name", elems)
-}
-
-func (h *HTML) Onclick(elems ...any) attr {
-	return attribute("onclick", elems)
-}
-
-func (h *HTML) Onsubmit(elems ...any) attr {
-	return attribute("onsubmit", elems)
-}
-
-func (h *HTML) Href(elems ...any) attr {
-	return attribute("href", elems)
-}
-
-func (h *HTML) Rel(elems ...any) attr {
-	return attribute("rel", elems)
-}
-
-func (h *HTML) Type(elems ...any) attr {
-	return attribute("type", elems)
-}
-
-func (h *HTML) Border(elems ...any) attr {
-	return attribute("border", elems)
-}
-
-func (h *HTML) Summary(elems ...any) attr {
-	return attribute("summary", elems)
-}
-
-func (h *HTML) Class(elems ...any) attr {
-	return attribute("class", elems)
-}
-
-func (h *HTML) Id(elems ...any) attr {
-	return attribute("id", elems)
-}
-
-func (h *HTML) Size(elems ...any) attr {
-	return attribute("size", elems)
-}
-
-func (h *HTML) Style(elems ...any) attr {
-	return attribute("style", elems)
-}
-
-func (h *HTML) Value(elems ...any) attr {
-	return attribute("value", elems)
-}
-
-func (h *HTML) Download(elems ...any) attr {
-	return attrNoArg("download", elems)
+func (h *HTML) Ul(elems ...any) *frag {
+	return tag("ul", elems)
 }
 
